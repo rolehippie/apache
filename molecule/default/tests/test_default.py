db479c41d7bcc18b6b6326ca656bd0bbefe5ab21
Thomas Boerger <thomas@webhippie.de>
2023-08-09 00:35:27 +0200

    ci: add tests for apache and exporter

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,5 +6,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-def test_dummy(host):
-    assert True
+def test_apache_running_and_enabled(host):
+    svc = host.service("apache2")
+    assert svc.is_running
+    assert svc.is_enabled
+
+def test_apache_listener(host):
+    assert host.socket("tcp://127.0.0.1:80").is_listening
+
+def test_exporter_running_and_enabled(host):
+    svc = host.service("apache-exporter")
+    assert svc.is_running
+    assert svc.is_enabled
+
+def test_exporter_listener(host):
+    assert host.socket("tcp://127.0.0.1:9117").is_listening
